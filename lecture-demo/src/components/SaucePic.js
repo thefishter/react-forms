7db02e1b575b67cf7d8f3873cab3567d47ebcdea
Fanny Jiang <jiangf@bu.edu>
2017-03-29 17:56:39 -0400

    got rid of extra folders

--- lecture-demo/src/components/SaucePic.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const backgroundImg = (sauce) => ({backgroundImage : `url(./src/assets/${sauce}.gif`})
-
-export default ({sauce}) => (
-	<div className="sauce-img" style={backgroundImg(sauce)}></div>
-)
